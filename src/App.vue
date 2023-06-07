c0077428d36098657fc6015fcce2c1c10cfbede2
Luden8 <5334422+Luden8@users.noreply.github.com>
2023-06-07 14:44:50 +0200

    update paretn's model value

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <h3>Parent v-model value</h3>
-  <pre>{{modeValue}}</pre>
-  <test-form v-model="modeValue"></test-form>
+  <pre>{{ modelValue }}</pre>
+  <button @click="handleSet">Change parent's state</button>
+  <br />
+  <br />
+  <test-form v-model="modelValue"></test-form>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
 import testForm from './shared/components/form/testForm.vue';
 
-const modeValue = ref({
+const modelValue = ref({
   name: '',
   testProp: ''
 });
+
+const handleSet = () =>
+  (modelValue.value = {
+    name: 'ttest',
+    testProp: 'testtest'
+  });
 </script>
